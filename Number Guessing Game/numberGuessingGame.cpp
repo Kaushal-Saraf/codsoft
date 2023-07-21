8d60f26a1d554ffe6fb8a71eb4310ef2f04141da
Kaushal <kaushalsaraf2002@gmail.com>
2023-07-21 18:52:19 +0530

    number guessing game file added

---
+++ Number Guessing Game/numberGuessingGame.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    srand(time(0));
+    int x = rand()%10;
+    int c=-1;
+    while(c!=x){
+        cout<<"Choose a number between 0 and 9:"<<" ";
+        cin>>c;
+        if(c>x){
+            cout<< "Try smaller number."<<endl;
+        }
+        else if(c<x){
+            cout<< "Try bigger number."<<endl;
+        }
+    }
+    cout<<"You guessed it right."<<endl;
+    system("pause");
+    return 0;
+}
