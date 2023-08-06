af5ec7629f2f19fa197f5b103405beff06d63378
Kaushal <kaushalsaraf2002@gmail.com>
2023-08-06 16:46:13 +0530

    Word count program added

---
+++ Word Count/wordCount.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string fileLocation;
+    cout<<"Please Enter the location of the File: ";
+    cin>>fileLocation;
+    ifstream inFile;
+    inFile.open(fileLocation);
+    string str="";
+    string fullstring= "";
+    int noOfWords=0;
+    while(getline(inFile,str))
+    {  
+        fullstring+=str+" ";
+    }
+    int i=0;
+    int totalLenght= fullstring.size();
+    while(i<=totalLenght)
+    {
+        if(fullstring[i]==' '){
+            noOfWords++;
+        }
+        i++;
+    }
+    cout<<"Total number of words in this file is : "<<noOfWords<<endl;
+    return 0;
+}
